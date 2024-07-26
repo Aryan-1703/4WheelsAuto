e44c01bd52a06c3501ee4a79bf25cac58828d647
Aryan-1703 <aryan17032@gmail.com>
2024-07-26 11:28:59 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,11 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-// Routes
-// Define your routes here or in separate route files
-app.use("/api", require("./routes/api"));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 // Connect to MongoDB
 mongoose
