ab8afede7fee9c70fd9d7868e73bcf22f28fe38f
Aryan <aryan.talpada@crosslinxtransit.ca>
2024-07-31 16:31:23 -0400

    Update

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-const PORT = process.env.SMTP_PORT || 3001;
+const PORT = process.env.SMTP_PORT ;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
