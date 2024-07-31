ec39d104ce8db52e68e14e260666fc99bbaca836
Aryan <aryan.talpada@crosslinxtransit.ca>
2024-07-31 15:37:20 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,8 @@ app.post("/api/send-email", async (req, res) => {
 	}
 });
 
-app.listen(3001, () => {
-	console.log("Server is running on port 3001");
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
