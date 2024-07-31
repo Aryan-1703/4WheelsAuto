3ee6aeafc879b6a65461eba50313c537d1554c9b
Aryan <aryan.talpada@crosslinxtransit.ca>
2024-07-31 13:52:53 -0400

    UPdae

--- src/components/Home/gallery.tsx
+++ src/components/Home/gallery.tsx
@@ -1,24 +1,31 @@
 import "../../styles/Home/gallery.css";
 
 const Gallery = () => {
-	const images = [
-		"../images/shop/1.jpg",
-		"../images/shop/2.jpg",
-		"../images/shop/3.jpg",
-		"../images/shop/4.jpg",
-		"../images/shop/5.jpg",
-		"../images/shop/6.jpg",
-		"../images/shop/7.jpg",
-		"../images/shop/8.jpg",
-	];
+	// const images = [
+	// 	"../images/shop/1.jpg",
+	// 	"../images/shop/2.jpg",
+	// 	"../images/shop/3.jpg",
+	// 	"../images/shop/4.jpg",
+	// 	"../images/shop/5.jpg",
+	// 	"../images/shop/6.jpg",
+	// 	"../images/shop/7.jpg",
+	// 	"../images/shop/8.jpg",
+	// ];
 	return (
 		<div className="gallery-container">
 			<h2>Working Gallery</h2>
 			<h4>Snapshots of our working gallery</h4>
-			<div className="image-grid">
-				{images.map((src, index) => (
-					<img key={index} src={src} alt={`Gallery Image ${index + 1}`} />
-				))}
+			<div className="image-wrapper">
+				<div className="image-grid">
+					<img src="../images/shop/1.jpg" alt="Gallery Image 1" />
+					<img src="../images/shop/2.jpg" alt="Gallery Image 2" />
+					<img src="../images/shop/3.jpg" alt="Gallery Image 3" />
+					<img src="../images/shop/4.jpg" alt="Gallery Image 4" />
+					<img src="../images/shop/5.jpg" alt="Gallery Image 5" />
+					<img src="../images/shop/6.jpg" alt="Gallery Image 6" />
+					<img src="../images/shop/7.jpg" alt="Gallery Image 7" />
+					<img src="../images/shop/8.jpg" alt="Gallery Image 8" />
+				</div>
 			</div>
 		</div>
 	);
