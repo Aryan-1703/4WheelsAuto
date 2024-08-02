aeb3e69f16ebe7de809d56a6779d11193367ac2c
Aryan <aryan.talpada@crosslinxtransit.ca>
2024-08-02 09:38:56 -0400

    UPdate

--- backend/emailUtils.js
+++ backend/emailUtils.js
@@ -1,6 +1,5 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
@@ -13,25 +12,25 @@ const transporter = nodemailer.createTransport({
 	},
 });
 
-export const sendEmail = (name, email, phone, date, service) => {
+const sendEmail = (name, email, phone, date, service) => {
 	const mailOptions = {
 		from: `"Appointment" <${process.env.SMTP_USER}>`,
 		to: "fourwheelsauto@hotmail.com",
 		subject: `Appointment Request from ${name}`,
 		text: `You have received a new appointment request from ${name}.\n\nDetails:\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\nDate: ${date}\nService: ${service}\n\nTo contact them, you can reply directly to this email.`,
 		html: `
-			<p><strong>New Appointment Request</strong></p>
-			<p>You have received a new appointment request from <strong>${name}</strong>.</p>
-			<p><strong>Details:</strong></p>
-			<p><strong>Date:</strong> ${date}</p>
-			<p><strong>Service Requested:</strong> ${service}</p>
-			<p><strong>Contact Information:</strong></p>
-            <p><strong>Phone:</strong> ${phone}</p>
-			<p><strong>Email:</strong> <a href="mailto:${email}">${email}</a></p>
-		    <p>To contact them, simply click the email address above to send a direct reply.</p>
-			<p>Best regards,</p>
-			<strong>Appointment Manager - Aryan</strong>
-		`,
+        <p><strong>New Appointment Request</strong></p>
+        <p>You have received a new appointment request from <strong>${name}</strong>.</p>
+	    <p><strong>Details:</strong></p>
+ 	    <p><strong>Date:</strong> ${date}</p>
+ 	    <p><strong>Service Requested:</strong> ${service}</p>
+ 	    <p><strong>Contact Information:</strong></p>
+        <p><strong>Phone:</strong> ${phone}</p>
+  	    <p><strong>Email:</strong> <a href="mailto:${email}">${email}</a></p>
+  	    <p>To contact them, simply click the email address above to send a direct reply.</p>
+  	    <p>Best regards,</p>
+   	   <strong>Appointment Manager - Aryan</strong>
+    `,
 	};
 
 	return new Promise((resolve, reject) => {
@@ -44,3 +43,5 @@ export const sendEmail = (name, email, phone, date, service) => {
 		});
 	});
 };
+
+module.exports = { sendEmail };
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import cors from "cors";
-import sendEmail from "sendEmail";
+import {sendEmail} from "./emailUtils.js";
 
 dotenv.config();
 
